b61cfa0b07d63e82841553a27a20df7c65f6f6a1
Thomas Little <64131759+thomasjlittle@users.noreply.github.com>
2023-09-04 11:55:56 -0700

    Add files via upload

--- holmes_hw1.py
+++ holmes_hw1.py
@@ -14,25 +14,26 @@ def combine_nodes(nodes, tolerance, leaf_size):
     #Get lists of nodes within tolerance using KDTree
     tree = KDTree(nodes, leaf_size=leaf_size, metric='euclidean')
     count = tree.query_radius(nodes, r=tolerance, count_only=False, return_distance=False)
+    return len(set(tuple(x) for x in count))
+
+    # #Sort lists so minimum number of nodes are formed
+    # original_count_and_idx = list(enumerate(count))
+    # sorted_count_and_idx = sorted(original_count_and_idx, key = lambda x: len(x[1]), reverse = True)
+    # sorted_count = [sublist for _, sublist in sorted_count_and_idx]
+    # sorted_idx = [idx for idx, _ in sorted_count_and_idx]
+
+    # #Loop through nodes with connections and group
+    # combined_nodes = set()
+    # for i, node in zip(sorted_idx, sorted_count):
+    #     if i not in combined_nodes:
+    #         node_set = set(node).difference(combined_nodes)
+    #         for node in node_set:
+    #             combined_nodes.add(node)
+    #         count[i] = tuple(node_set)
+    #     else:
+    #         count[i] = ()
     
-    #Sort lists so minimum number of nodes are formed
-    original_count_and_idx = list(enumerate(count))
-    sorted_count_and_idx = sorted(original_count_and_idx, key = lambda x: len(x[1]), reverse = True)
-    sorted_count = [sublist for _, sublist in sorted_count_and_idx]
-    sorted_idx = [idx for idx, _ in sorted_count_and_idx]
-
-    #Loop through nodes with connections and group
-    combined_nodes = set()
-    for i, node in zip(sorted_idx, sorted_count):
-        if i not in combined_nodes:
-            node_set = set(node).difference(combined_nodes)
-            for node in node_set:
-                combined_nodes.add(node)
-            count[i] = tuple(node_set)
-        else:
-            count[i] = ()
-
-    return len(set(x for x in count if x != ()))
+        # return len(set(x for x in count if x != ()))
 
 def write_to_csv(nodes):
     with open('output.txt', "w", newline="") as file:
@@ -43,7 +44,7 @@ def write_to_csv(nodes):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_path', type=str, default='hw-1.json')
-    parser.add_argument('--tolerance', type=float, default=0.1)
+    parser.add_argument('--tolerance', type=float, default=0.05)
     parser.add_argument('--leaf_size', type=int, default=40)
     args = parser.parse_args()
     nodes = load_json(args.data_path)
